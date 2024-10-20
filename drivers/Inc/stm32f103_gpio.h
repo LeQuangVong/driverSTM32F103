c2489fa3d1cd8a9614785d2350e97f2c1190e718
LeQuangVong <vongoke@gmail.com>
2024-10-21 00:15:18 +0700

    C4 - Update driverSTM32F103

--- drivers/Inc/stm32f103_gpio.h
+++ drivers/Inc/stm32f103_gpio.h
@@ -30,10 +30,31 @@ typedef struct{
 #define PIN13 	13
 #define PIN14 	14
 
+/*
+    GPIO_t options are:
+    - pin: PINx, where x = 0 .. 15
+    - mode: 1) MODE_INPUT
+            2) MODE_OUTPUT_SPEED_10
+            3) MODE_OUTPUT_SPEED_2
+            4) MODE_OUTPUT_SPEED_50
+    - configuration:
+      for input mode:
+            1) CONFIG_INPUT_ANALOG
+            2) CONFIG_INPUT_FLOATING
+            3) CONFIG_INPUT_PULL_UP_DOWN
+      for output mode:
+            1) CONFIG_OUTPUT_GENERAL_PUSH_PULL
+            2) CONFIG_OUTPUT_GENERAL_OPEN_DRAIN
+            3) CONFIG_OUTPUT_ALTERNATE_FUNCTION_PUSH_PULL
+            4) CONFIG_OUTPUT_ALTERNATE_FUNCTION_OPEN_DRAIN
+    - port: PORTx where x = A B ... G
+*/
+
 #define CONFIG_INPUT_ANALOG									0x00000000 //MODE: 00, CNF: 00
 #define CONFIG_INPUT_FLOATING								0x00000004 //MODE: 00, CNF: 01
 #define CONFIG_INPUT_PULL_UP								0x00000010 //MODE: 00, CNF: 10, ODR: 1
 #define CONFIG_INPUT_PULL_DOWN								0x00000020 //MODE: 00, CNF: 10, ODR: 0
+#define CONFIG_INPUT_PULL_UP_PULL_DOWN						0x00000008 //MODE: 00, CNF: 10
 
 #define CONFIG_OUTPUT_GENERAL_PUSH_PULL						0x00000000 //MODE: 00, CNF: 00
 #define CONFIG_OUTPUT_GENERAL_OPEN_DRAIN 					0x00000004
--- drivers/Src/stm32f103_gpio.c
+++ drivers/Src/stm32f103_gpio.c
@@ -51,24 +51,54 @@ void PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 {
-	PeriClockControl(pGPIOHandle->pGPIOx,ENABLE);
+	PeriClockControl(pGPIOHandle->pGPIOx,ENABLE);//Enable Port
 
 	uint32_t modeConfig;
 	uint32_t updatedPin;
 	uint32_t pinLoopPosition, realPosition, currentPosition;
 	uint32_t temp;
 
-	uint8_t pullUpFlag = 0;
-	uint32_t finalConfig = pGPIOHandle->GPIO_PinConfig.PinConfig;
+	uint32_t finalConfig = pGPIOHandle->GPIO_PinConfig.PinConfig;//Lưu config vào biến
+
+	/*
+		Description: Kiểm tra xem mode phải là mode input hay không và thuộc pullup hay pulldown
+					- Nếu pullup thì set bit tương ứng với chân lên bằng 1
+					- Nếu là pulldown thì reset bit tương ứng với chân bằng 0.
+	*/
 
 	if(pGPIOHandle->GPIO_PinConfig.PinMode == MODE_INPUT && (pGPIOHandle->GPIO_PinConfig.PinConfig == CONFIG_INPUT_PULL_UP || pGPIOHandle->GPIO_PinConfig.PinConfig == CONFIG_INPUT_PULL_DOWN))
 	{
 		if(pGPIOHandle->GPIO_PinConfig.PinConfig == CONFIG_INPUT_PULL_UP)
 		{
-			pullUpFlag = 1;
+			//pullUpFlag = 1;
+			pGPIOHandle->pGPIOx->ODR |= (1 << pGPIOHandle->GPIO_PinConfig.PinNumber);
+		}else if(pGPIOHandle->GPIO_PinConfig.PinConfig == CONFIG_INPUT_PULL_DOWN)
+		{
+			pGPIOHandle->pGPIOx->ODR &= ~(1 << pGPIOHandle->GPIO_PinConfig.PinNumber);
 		}
-		finalConfig = CONFIG_INPUT_PULL_DOWN;
+		finalConfig = CONFIG_INPUT_PULL_UP_PULL_DOWN;
 	}
+
+	/*
+			Description: Cấu hình cho thanh ghi GPIOx_CRL, Port x configuration bits (y= 0 .. 7)
+						- lặp từ 0->7 tương ứng với 8 pin: 1pin có 4 bits
+						- Tìm vị trí của vòng lặp tại pin nào
+						- Tìm vị trí hiện tại của pin đang cài đặt
+						- nếu vị trí của vòng lặp bằng vị trí hiện tại của pin đang cài đặt
+							+ Xóa cấu hình hiện tại của chân
+							+ Thiết lập cấu hình mới
+							+ cập nhật thanh ghi GPIOx_CRL
+
+			Example: realPosition = 0x04  (0x01 << 2): 0x0100
+					pGPIOHandle->GPIO_PinConfig.PinNumber = 2 (Pin_2): 0x0100
+					currentPosition = 0x0100 : realPosition & 0x0100 = 0x0100
+
+					GPIOx_CRL: 0000 0000 0000 0000 0000 0000 0000 0000
+					GPIO_PinConfig.PinMode = MODE_INPUT: 0x00
+					finalConfig = 0x10 ==> modeConfig = 0x10 | 0x00 = 0x10
+					temp = 0x100000000000;
+					GPIOx_CRL: 0000 0000 0000 0000 0000 1000 0000 0000
+	*/
 
 	for(pinLoopPosition = 0x00; pinLoopPosition < 0x08; pinLoopPosition++)
 	{
@@ -84,6 +114,27 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 		}
 	}
 
+	/*
+				Description: Cấu hình cho thanh ghi GPIOx_CRH, Port x configuration bits (y= 8 .. 15)
+							- lặp từ 8->15 tương ứng với 8 pin: 1pin có 4 bits
+							- Tìm vị trí của vòng lặp tại pin nào
+							- Tìm vị trí hiện tại của pin đang cài đặt
+							- nếu vị trí của vòng lặp bằng vị trí hiện tại của pin đang cài đặt
+								+ Xóa cấu hình hiện tại của chân
+								+ Thiết lập cấu hình mới
+								+ cập nhật thanh ghi GPIOx_CRH
+
+				Example: realPosition = 0x04  (0x01 << 10): 0x10000000000
+						pGPIOHandle->GPIO_PinConfig.PinNumber = 10 (Pin_10): 0x10000000000
+						currentPosition = 0x10000000000 : realPosition & 0x10000000000 = 0x10000000000
+
+						GPIOx_CRH: 0000 0000 0000 0000 0000 0000 0000 0000
+						GPIO_PinConfig.PinMode = MODE_INPUT: 0x00
+						finalConfig = 0x10 ==> modeConfig = 0x10 | 0x00 = 0x10
+						temp = 0x100000000000;
+						GPIOx_CRH: 0000 0000 0000 0000 0000 1000 0000 0000
+		*/
+
 	for(pinLoopPosition = 0x08; pinLoopPosition < 0x10; pinLoopPosition++)
 		{
 			realPosition = (0x01)<<pinLoopPosition;
@@ -98,12 +149,6 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 				pGPIOHandle->pGPIOx->CRH = temp;
 			}
 		}
-
-	if(pullUpFlag == 1)
-	{
-		pullUpFlag = 0;
-		pGPIOHandle->pGPIOx->BSRR = pGPIOHandle->GPIO_PinConfig.PinNumber;
-	}
 }
 
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
@@ -126,6 +171,12 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 	}
 }
 
+/*
+ * Description: Đọc trạng thái của các chân
+ * Example: pinNumber = 5
+			pGPIOx->IDR = 0x0000000000100000
+			pGPIOx->IDR >> 5 = 0x01 & 0x00000001 = 1
+ */
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber)
 {
 	uint8_t value;
@@ -133,12 +184,24 @@ uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber)
 	return value;
 }
 
+/*
+ * Description: Đọc trạng thái của Port
+ * Example: pinNumber = 5
+			pGPIOx->IDR = 0x0000000000100000
+ */
+
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
 {
 	uint16_t value;
 	value = (uint16_t)(pGPIOx->IDR);
 	return value;
 }
+
+/*
+ * Description: Viết giá trị vào pin
+				+ Nếu value == 1, thì set bit tương ứng với chân trong thanh ghi ODR
+				+ Ngược lại thì reset bit tương ứng
+ */
 
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t value, uint8_t pinNumber)
 {
@@ -150,6 +213,11 @@ void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t value, uint8_t pinNumb
 		pGPIOx->ODR &=~ (1<<pinNumber);
 	}
 }
+
+/*
+ * Description: Viết giá trị vào Port
+				+ Nếu bit nào trong value ở low hoặc high thì sẽ reset hoặc set bit ứng với chân
+ */
 
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint8_t value)
 {
