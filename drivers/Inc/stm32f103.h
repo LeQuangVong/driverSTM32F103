de05ae88c6bbbb9e3110a70432c1272b1a123b81
LeQuangVong <vongoke@gmail.com>
2024-10-27 00:22:39 +0700

    C3 update SPI driver

--- drivers/Inc/stm32f103.h
+++ drivers/Inc/stm32f103.h
@@ -153,6 +153,10 @@ typedef struct{
 #define IRQ_NO_EXTI9_5			23
 #define IRQ_NO_EXTI15_10		40
 
+#define IRQ_NO_SPI1				35
+#define IRQ_NO_SPI2				36
+#define IRQ_NO_SPI3				51
+
 #define SPI_CR1_CPHA			0
 #define SPI_CR1_CPOL			1
 #define SPI_CR1_MSTR			2
--- drivers/Inc/stm32f103_spi.h
+++ drivers/Inc/stm32f103_spi.h
@@ -2,6 +2,7 @@
 #define INC_STM32F103_SPI_H_
 
 #include "stm32f103.h"
+#include <stddef.h>
 
 typedef struct{
 	uint8_t SPI_DeviceMode;
@@ -24,6 +25,11 @@ typedef struct{
 	uint8_t  RxState;
 }SPI_Handle_t;
 
+#define SPI_EVENT_TX_CMPLT			1
+#define SPI_EVENT_RX_CMPLT			2
+#define SPI_EVENT_OVR_ERR			3
+#define SPI_EVENT_CRC_ERR			4
+
 #define SPI_READY					0
 #define SPI_BUSY_IN_RX				1
 #define SPI_BUSY_IN_TX				2
@@ -35,14 +41,15 @@ typedef struct{
 #define	SPI_BUS_CONFIG_HD			2
 #define SPI_BUS_CONFIG_SIMPLEX		3
 
-#define SPI_SCLK_SPEED_DIV2				0
-#define SPI_SCLK_SPEED_DIV4				1
-#define SPI_SCLK_SPEED_DIV8				2
-#define SPI_SCLK_SPEED_DIV16			3
-#define SPI_SCLK_SPEED_DIV32			4
-#define SPI_SCLK_SPEED_DIV64			5
-#define SPI_SCLK_SPEED_DIV128			6
-#define SPI_SCLK_SPEED_DIV256			7
+//serial clock baud rate					BR[2:0]
+#define SPI_SCLK_SPEED_DIV2				0	//000
+#define SPI_SCLK_SPEED_DIV4				1	//001
+#define SPI_SCLK_SPEED_DIV8				2	//010
+#define SPI_SCLK_SPEED_DIV16			3	//011
+#define SPI_SCLK_SPEED_DIV32			4	//100
+#define SPI_SCLK_SPEED_DIV64			5	//101
+#define SPI_SCLK_SPEED_DIV128			6	//110
+#define SPI_SCLK_SPEED_DIV256			7	//111
 
 #define SPI_DFF_8BITS				0
 #define SPI_DFF_16BITS				1
@@ -67,11 +74,20 @@ uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName);
 void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
 
-uint8_t SPI_ReceiveDataIT(SPI_Handle_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
-uint8_t SPI_SendDataIT(SPI_Handle_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+
+uint8_t SPI_ReceiveDataIT(SPI_Handle_t *pSPIHandle, uint8_t *pTxBuffer, uint32_t Len);
+uint8_t SPI_SendDataIT(SPI_Handle_t *pSPIHandle, uint8_t *pTxBuffer, uint32_t Len);
 
 void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
 void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
 void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
+
+void SPI_ClearOVRFlag(SPI_RegDef_t *pSPIx);
+
+void SPI_CloseTransmission(SPI_Handle_t *pSPIHandle);
+void SPI_CloseReception(SPI_Handle_t *pSPIHandle);
+void SPI_ApplicationEventCallback(SPI_Handle_t *pSPIHandle, uint8_t AppEv);
 
 #endif /* INC_STM32F103_SPI_H_ */
